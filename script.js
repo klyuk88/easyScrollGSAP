88bf5adfe09345716eb7565786a8ae7943578c47
Ilya Klyukovskiy <48031577+klyuk88@users.noreply.github.com>
2021-10-16 13:05:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,6 @@
     }
 
     easyScroll()
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/ScrollToPlugin.min.js"></script>
